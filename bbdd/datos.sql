04a02486ab7804e7786936549e0e3ce09ff66d9c
carlostoquero <carlostoquero@gmail.com>
2017-08-01 19:22:39 +0200

    BACKEND -> Scripts

---
+++ bbdd/datos.sql
@@ -0,0 +1,14 @@
+INSERT INTO ACCESO_USUARIO(id_acceso, nombre_acceso) VALUES (1, 'Usuario');
+INSERT INTO ACCESO_USUARIO(id_acceso, nombre_acceso) VALUES (2, 'Administrador');
+
+INSERT INTO ESTADO_USUARIO(id_estado, nombre_estado) VALUES (0, 'PENDIENTE');
+INSERT INTO ESTADO_USUARIO(id_estado, nombre_estado) VALUES (1, 'VALIDADO');
+INSERT INTO ESTADO_USUARIO(id_estado, nombre_estado) VALUES (2, 'RECHAZADO');
+
+INSERT INTO TIPO_COMPETICION(id_tipo_competicion, nombre_tipo_competicion) VALUES (1, 'Liga');
+INSERT INTO TIPO_COMPETICION(id_tipo_competicion, nombre_tipo_competicion) VALUES (2, 'Copa');
+INSERT INTO TIPO_COMPETICION(id_tipo_competicion, nombre_tipo_competicion) VALUES (3, 'Mixto');
+
+INSERT INTO TIPO_JORNADA(id_tipo_jornada, nombre_tipo_jornada) VALUES (1, 'Liga');
+INSERT INTO TIPO_JORNADA(id_tipo_jornada, nombre_tipo_jornada) VALUES (2, 'Eliminatoria');
+
---
+++ bbdd/estructura.sql
@@ -0,0 +1,4 @@
+CREATE TABLE ACCESO_USUARIO(id_acceso int, nombre_acceso varchar(45), primary key(id_acceso));
+CREATE TABLE ESTADO_USUARIO(id_estado int, nombre_acceso varchar(45), primary key(id_estado));
+CREATE TABLE TIPO_COMPETICION(id_tipo_competicion int, nombre_tipo_competicion varchar(45), primary key(id_tipo_competicion));
+CREATE TABLE TIPO_JORNADA(id_tipo_jornada int, nombre_tipo_jornada varchar(45), primary key(id_tipo_jornada));
