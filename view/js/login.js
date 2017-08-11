f18022232ab99de8c42aeae8598f5c9721111db8
carlostoquero <carlostoquero@gmail.com>
2017-08-11 10:07:15 +0200

    LOGIN Implementación login

--- view/js/login.js
+++ view/js/login.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
 	$('.btn_login').on('click', function(){
-		location.href = './reglas.html';
+		$('.errores').html('');
+		if (checkInput()){
+			var login = $('#in_user').val();
+			var password = $('#in_password').val();
+			
+			var correctLogin = getAjaxSync('ServicioUsuarios', 'Login', JSON.stringify({login: login, password : password}));
+			if (correctLogin === "ok") location.href = './reglas.html';
+			else $('.errores').append('<div>Login incorrecto.</div>');
+		}
 	});
 	generarFooter();
+	
+	function checkInput(){
+		var correct_input = true;
+		if ($('#in_user').val() === null || $('#in_user').val() === ""){
+			correct_input = false;
+			$('.errores').append('<div>Debe informarse el nombre de usuario</div>');
+		} 
+		if ($('#in_password').val() === null || $('#in_password').val() === ""){
+			correct_input = false;
+			$('.errores').append('<div>Debe informarse la contraseña</div>');
+		} 
+		return correct_input;
+
+	}
 });
