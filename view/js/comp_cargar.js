bf3e5a4fcfa09b0dde338f76aa7847ce3b435635
carlostoquero <carlostoquero@gmail.com>
2017-08-29 14:32:51 +0200

    COMPETICION -> CARGAR Gestión jornada actual

--- view/js/comp_cargar.js
+++ view/js/comp_cargar.js
@@ -9,25 +9,29 @@ $(document).ready(function(){
 	generarFooter();
 	
 	$('body').on('click','.selector-jornada', function(){
-		jornada_seleccionada = parseInt($(this).attr('data-id-jornada'));
-		partidos = getPartidosJornada(jornada_seleccionada);
-		if (partidos && partidos.length > 0){
-			$('.tabla-partidos').find('tr').remove();
-			$.each(partidos, function(index, partido){
-				var equipo_1 = findElementByField(equipos, "id_equipo", partido.id_equipo_1);
-				var equipo_2 = findElementByField(equipos, "id_equipo", partido.id_equipo_2);
-				if (equipo_1 !== null && equipo_2 !== null){
-					var row = $('<tr class="jornada">');
-					row.append('<td><img src="' + equipo_1.url_escudo + '" title="' + equipo_1.nombre_equipo + '"></td>');
-					row.append('<td>' + equipo_1.abreviatura + '</td>');
-					row.append('<td><input type="text" value="' + partido.goles_equipo_1 + '" /></td>');
-					row.append('<td>-</td>');
-					row.append('<td><input type="text" value="' + partido.goles_equipo_2 + '" /></td>');
-					row.append('<td>' + equipo_2.abreviatura + '</td>');
-					row.append('<td><img src="' + equipo_2.url_escudo + '" title="' + equipo_2.nombre_equipo + '"></td>');
-					$('.tabla-partidos').append(row);
-				}
-			});
+		var idJornada = parseInt($(this).attr('data-id-jornada'));
+		jornada_seleccionada = findElementByField(jornadas, "id_jornada", idJornada);
+		if (jornada_seleccionada !== null){
+			$('.jornada.title').html('Resultados - ' + jornada_seleccionada.nombre_jornada);
+			partidos = getPartidosJornada(jornada_seleccionada.id_jornada);
+			if (partidos && partidos.length > 0){
+				$('.tabla-partidos').find('tr').remove();
+				$.each(partidos, function(index, partido){
+					var equipo_1 = findElementByField(equipos, "id_equipo", partido.id_equipo_1);
+					var equipo_2 = findElementByField(equipos, "id_equipo", partido.id_equipo_2);
+					if (equipo_1 !== null && equipo_2 !== null){
+						var row = $('<tr class="jornada">');
+						row.append('<td><img src="' + equipo_1.url_escudo + '" title="' + equipo_1.nombre_equipo + '"></td>');
+						row.append('<td>' + equipo_1.abreviatura + '</td>');
+						row.append('<td><input type="text" value="' + partido.goles_equipo_1 + '" /></td>');
+						row.append('<td>-</td>');
+						row.append('<td><input type="text" value="' + partido.goles_equipo_2 + '" /></td>');
+						row.append('<td>' + equipo_2.abreviatura + '</td>');
+						row.append('<td><img src="' + equipo_2.url_escudo + '" title="' + equipo_2.nombre_equipo + '"></td>');
+						$('.tabla-partidos').append(row);
+					}
+				});
+			}
 		}
 	});
 
@@ -38,7 +42,8 @@ $(document).ready(function(){
 	$.each(jornadas, function(index, jornada){
 		$('.jornadas').append($('<div class="selector-jornada" data-id-jornada="' + jornada.id_jornada + '"><a href="#" data-numero-jornada="' + jornada.numero_jornada + '">' + jornada.nombre_corto + '</a></div>'));
 	});
-	jornada_seleccionada = getJornadaActual();
-	$('.selector-jornada[data-id-jornada="' + jornada_seleccionada + '"]').trigger('click');
+	
+	jornada_seleccionada = getJornadaActual(competicion_seleccionada.id_competicion);
+	$('.selector-jornada[data-id-jornada="' + jornada_seleccionada.id_jornada + '"]').trigger('click');
 	
 });
